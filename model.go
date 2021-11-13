844ae7f6dca1c1e45c4c3f3bdabdf4ff9315a49c
lemo <lemo@gamstudio.net>
2021-11-13 18:18:09 +0800

    1.update to v8.

--- model.go
+++ model.go
@@ -28,30 +28,30 @@ func (p *Model) SetHandler(r *Client) *Model {
 	return p
 }
 
-func (p *Model) key(id int) string {
+func (p *Model) Key(id int) string {
 	return p.Name + ":" + strconv.Itoa(id)
 }
 
 func (p *Model) Get(id int) *redis.StringStringMapCmd {
-	return p.Cache.Handler.HGetAll(context.Background(), p.key(id))
+	return p.Cache.Handler.HGetAll(context.Background(), p.Key(id))
 }
 
 func (p *Model) Delete(id int) *redis.IntCmd {
-	return p.Cache.Handler.Del(context.Background(), p.key(id))
+	return p.Cache.Handler.Del(context.Background(), p.Key(id))
 }
 
 func (p *Model) Create(id int, value map[string]interface{}) *redis.BoolCmd {
-	return p.Cache.Handler.HMSet(context.Background(), p.key(id), value)
+	return p.Cache.Handler.HMSet(context.Background(), p.Key(id), value)
 }
 
 func (p *Model) Update(id int, update map[string]interface{}) *redis.BoolCmd {
-	return p.Cache.Handler.HMSet(context.Background(), p.key(id), update)
+	return p.Cache.Handler.HMSet(context.Background(), p.Key(id), update)
 }
 
 func (p *Model) Expire(id int, time time.Duration) *redis.BoolCmd {
-	return p.Cache.Handler.Expire(context.Background(), p.key(id), time)
+	return p.Cache.Handler.Expire(context.Background(), p.Key(id), time)
 }
 
 func (p *Model) Exists(id int) *redis.IntCmd {
-	return p.Cache.Handler.Exists(context.Background(), p.key(id))
+	return p.Cache.Handler.Exists(context.Background(), p.Key(id))
 }
